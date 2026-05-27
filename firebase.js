bee122343f9b6f2e287812898a6957769ba2053e
Yanatorn <morestudio.no@gmail.com>
2026-05-27 13:02:42 +0700

    Integrate Azure DevOps, dynamic dashboard, and API status endpoint

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  projectId: "morestudio-sprint-2026",
+  appId: "1:97508017044:web:0707e3f2138ed43f8a0581",
+  storageBucket: "morestudio-sprint-2026.firebasestorage.app",
+  apiKey: "AIzaSyDGmV4wQlF1WEJFkihZDULU9kT7BTvsm8Y",
+  authDomain: "morestudio-sprint-2026.firebaseapp.com",
+  messagingSenderId: "97508017044"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
