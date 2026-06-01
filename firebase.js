9c2be91927c01b584c5dbe0dfd5fa2f576649469
Yanatorn <morestudio.no@gmail.com>
2026-06-01 13:09:19 +0700

    Revert "Force long polling instead of WebChannel to fix 400 errors"
    This reverts commit 0fdf184e886fe3d8daa7062d5a7696b47e0b9971.

--- firebase.js
+++ firebase.js
@@ -12,6 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-  useFetchStreams: false,
+  experimentalAutoDetectLongPolling: true,
 });
